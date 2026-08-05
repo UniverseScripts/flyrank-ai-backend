b919d843d9222b775819a8a51453137ba0d18a2f
Yoshio <universegaming234@gmail.com>
2026-08-05 18:07:43 +0700

    Stage 3: create with validation

--- w2/main.py
+++ w2/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException, status
 from json import load
+from pydantic import BaseModel
 import os
 
 with open(os.path.join(os.path.dirname(__file__), 'mock_data.json'), 'r') as f:
@@ -8,23 +9,36 @@ with open(os.path.join(os.path.dirname(__file__), 'mock_data.json'), 'r') as f:
 app = FastAPI(title="Flyrank AI", description="Flyrank AI Backend", version="0.0.1")
 
 @app.get("/")
-def read_root():
+async def read_root():
     return { "name": "Task API", "version": "1.0", "endpoints": ["/tasks"] }
 
 @app.get('/health')
-def check_health():
+async def check_health():
     return { "status": "ok" }
 
 @app.get('/tasks')
-def get_tasks():
+async def get_tasks():
     return data["tasks"]
 
 @app.get('/tasks/{id}')
-def get_task(id: int):
+async def get_task(id: int):
     for task in data["tasks"]:
         if task["id"] == id:
             return task
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Task {id} not found")
+
+class TaskCreate(BaseModel):
+    title: str
+
+@app.post('/tasks')
+async def create_task(task: TaskCreate, status_code: int = status.HTTP_201_CREATED):
+    new_task = {
+        "id": len(data["tasks"]) + 1,
+        "title": task.title,
+        "done": False
+    }
+    data["tasks"].append(new_task)
+    return {"task": new_task }
 
 if __name__ == "__main__":
     import uvicorn
